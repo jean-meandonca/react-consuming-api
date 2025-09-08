b3ab73f881348743f1a343386cd4e5452156815b
Jean <jeannguilherme30@gmail.com>
2025-09-08 20:00:47 -0300

    Css dos formulario, listagem e botao

--- devclub-cadastro-usuarios/pages/Home/index.jsx
+++ devclub-cadastro-usuarios/pages/Home/index.jsx
@@ -4,44 +4,44 @@ import Trash from '../../src/assets/trash.svg'
 function Home() {
 
   const users = [
-  {
-    id: 'FEFwefwefwe',
-    name: 'Rodolfo',
-    age: 33,
-    email: 'rod@email.com'
-  },
-  { 
-    id: 'jjjretfwefwe',
-    name: 'Aline',
-    age: 35,
-    email: 'Ali@email.com',
-  },]
+    {
+      id: 'FEFwefwefwe',
+      name: 'Rodolfo',
+      age: 33,
+      email: 'rod@email.com'
+    },
+    {
+      id: 'jjjretfwefwe',
+      name: 'Aline',
+      age: 35,
+      email: 'Ali@email.com',
+    },]
 
   return (
     <div className='container'>
 
       <form action="">
         <h1>Cadastro de Usuários</h1>
-        <input name='nome' type='text' />
-        <input name='idade' type='number' />
-        <input name='email' type='email' />
+        <input placeholder="Nome" name='nome' type='text' />
+        <input placeholder="Idade" name='idade' type='number' />
+        <input placeholder="E-mail" name='email' type='email' />
         <button type='button'>Cadastrar</button>
       </form>
 
-    {users.map( user => (
-      <div key={user.id}>
-        <div>
-          <p>Nome: {user.name} </p>
-          <p>Idade:{user.age} </p>
-          <p>Email:{user.email} </p>
-          <button>
-            <img src={Trash} alt="" />
-          </button>
+      {users.map(user => (
+        <div key={user.id} className="card">
+          <div>
+            <p>Nome: <span>{user.name}</span> </p>
+            <p>Idade:<span>{user.age}</span> </p>
+            <p>Email:<span>{user.email}</span> </p>
+          </div>
+           <button>
+              <img src={Trash} alt="" />
+            </button>
         </div>
-      </div>
-    ))}
+      ))}
 
-      
+
     </div>
   )
 }
